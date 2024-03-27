0588083541001d1738a29062104e5903ab1e4063
Daniel da Cunha Pestana <123014174+danielcpestana@users.noreply.github.com>
2024-03-27 08:47:59 -0300

    tt

--- BitcoiNatv/App.js
+++ BitcoiNatv/App.js
@@ -6,6 +6,44 @@ import HistoryGraphic from './src/components/HistoryGraphic/';
 import QuotationsList from './src/components/QuotationsList/';
 import Quotationitem from './src/components/QuotationsList/Quotationitems';
 
+function addZero(number){
+  if(number <= 9){
+    return "0" + number
+  }
+}
+
+
+function url(qtdDays){
+  const date = new Date();
+  const listLastDays = qtdDays
+  const end_date = 
+  `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}`
+  date.setDate(date.getDate()- listLastDays)
+  const start_date = 
+  `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}`
+
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`
+
+}
+
+
+async function getListCoins(url){
+  let response = await fetch(url);
+  let returnApi = await response.json()
+  let selectListQuotations = returnApi.bpi
+  const queryCoinsList = Object.keys(selectListQuotations.map((key)=>{
+    return {
+      data: key.split("-").reverse().join("/"),
+      valor: selectListQuotations[key]
+    }
+  })
+  let data = queryCoinsList.reverse();
+  return data
+
+}
+
+
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
